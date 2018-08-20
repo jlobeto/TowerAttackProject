81ef829dac2c926e8e6fdf6aba1c4ab8b40ab01c
Julian Lobeto <julian.lobeto93@gmail.com>
2018-08-20 17:32:59 -0300

    Added testController so that we can restart the level ingame (only for debug builds)

---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class TestController : MonoBehaviour
+{
+    GUIStyle _guiStyle = new GUIStyle(); 
+    // Use this for initialization
+    void Awake ()
+    {
+        if (!Debug.isDebugBuild)
+            Destroy(gameObject);
+
+        _guiStyle.fontSize = 12;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical();    
+        GUILayout.Label("R: reset scene", _guiStyle);
+        GUILayout.EndVertical();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Sandbox Scene");
+        }
+	}
+}
