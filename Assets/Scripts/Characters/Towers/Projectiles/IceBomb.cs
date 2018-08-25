9a276ddd67239c29117177b5a75e287902dfd2b4
Julian Lobeto <julian.lobeto93@gmail.com>
2018-08-25 00:07:14 -0300

    level prefab y icebomb.cs

---
+++ Assets/Scripts/Characters/Towers/Projectiles/IceBomb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBomb : Bomb
+{
+    [Range(0f,1f)]
+    public float deltaSpeed = 0.4f;
+    public float slowTime = 3f;
+
+    protected override void DoDamage(Minion m)
+    {
+        base.DoDamage(m);
+        m.GetSlowDebuff(slowTime, deltaSpeed);
+    }
+}
