b4072b0132921ff88ffe5e32a1296ce0eaeda9a9
Julian Lobeto <julian.lobeto93@gmail.com>
2018-10-15 13:57:30 -0300

    Particle sys en la spawn point de las laser turrets.

--- Assets/Scripts/Characters/Towers/LaserTower.cs
+++ Assets/Scripts/Characters/Towers/LaserTower.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LaserTower : TowerBase
 {
+    public Transform upSpawn;
+    public Transform secondGrounSpawn;
+    public ParticleSystem laserSpawnPS;
 
     Minion _target;
     LineRenderer _lineRender;
@@ -11,6 +14,7 @@ public class LaserTower : TowerBase
     float _timeShooting = 1;
     float _timeShootingAux = 1;
     float _targetHPToGo; //this is the minion hp that is going to have after 'timeShooting' time.
+    bool _leftSpawn;
 
     protected override void Start()
     {
@@ -39,9 +43,12 @@ public class LaserTower : TowerBase
         if (pTarget == null)
         {
             if (_lineRender.enabled)
+            {
+                laserSpawnPS.Stop();
                 _lineRender.enabled = false;
-
+            }
             _timeShooting = _timeShootingAux;
+            
             return;
         }
 
@@ -49,6 +56,7 @@ public class LaserTower : TowerBase
         {
             _target = pTarget.GetComponent<Minion>();
             _lineRender.enabled = true;
+            laserSpawnPS.Play();
             //_targetHPToGo = _target.hp - pMyStat.dmgPerSecond;
             _target.GetDamage(pMyStat.dmgPerSecond);
         }
@@ -63,16 +71,43 @@ public class LaserTower : TowerBase
             _target.GetDamage(pMyStat.dmgPerSecond);
         }
 
-        _lineRender.SetPosition(0, spawnPoint.position);
         if (_target != null)
+        {
+            if (_target.targetType == TargetType.Ground)
+            {
+                if (_leftSpawn)
+                {
+                    laserSpawnPS.transform.position = spawnPoint.position;
+                    _lineRender.SetPosition(0, spawnPoint.position);
+                }
+                else
+                {
+                    laserSpawnPS.transform.position = secondGrounSpawn.position;
+                    _lineRender.SetPosition(0, secondGrounSpawn.position);
+                }
+            }
+            else
+            {
+                laserSpawnPS.transform.position = upSpawn.position;
+                _lineRender.SetPosition(0, upSpawn.position);
+            }
+
+
+
             _lineRender.SetPosition(1, _target.transform.position);
+        }
         else
+        {
+            _leftSpawn = !_leftSpawn;
             pTarget = null;
+        }
+            
     }
 
     protected override void OnTargetHasChanged()
     {
         //Debug.Log("OnTargetHasChanged()");
+        _leftSpawn = !_leftSpawn;
         _target = pTarget.GetComponent<Minion>();
     }
 
