f207e18ef56e2309acb914bd7fd0c5d6637f8094
Julian Lobeto <julian.lobeto93@gmail.com>
2018-10-21 23:11:19 -0300

    Feedback del minion path, muy performante(?

--- Assets/Scripts/Level/LevelEvents/Weather/RainColliderManager.cs
+++ Assets/Scripts/Level/LevelEvents/Weather/RainColliderManager.cs
@@ -8,6 +8,7 @@ public class RainColliderManager : MonoBehaviour
     List<HitAreaCollider> _hitAreaColliders;
 
     List<Collider> _colliders;
+    List<MeshRenderer> _meshModified;
 
     HitAreaCollider _selected;
 
@@ -15,6 +16,7 @@ public class RainColliderManager : MonoBehaviour
 
 	void Start ()
     {
+        _meshModified = new List<MeshRenderer>();
         _hitAreaColliders = GetComponentsInChildren<HitAreaCollider>().ToList();
         _colliders = GetComponentsInChildren<Collider>().ToList();
         foreach (var item in _hitAreaColliders)
@@ -31,6 +33,7 @@ public class RainColliderManager : MonoBehaviour
 
     public HitAreaCollider SelectArea(float speedDelta)
     {
+        _meshModified = new List<MeshRenderer>();
         _selected = _hitAreaColliders[Random.Range(0, _hitAreaColliders.Count)];
         _selected.GetComponent<Collider>().enabled = true;
         _speedDelta = speedDelta;
@@ -41,25 +44,49 @@ public class RainColliderManager : MonoBehaviour
     {
         _selected.GetComponent<Collider>().enabled = false;
         _selected = null;
-    }
-
-    void TriggerEnterHandler(Collider col)
-    {
-        if (col.gameObject.layer != LayerMask.NameToLayer("Minion")) return;
-
-        //Debug.Log("TriggerEnterHandler");
-        var minion = col.GetComponent<Minion>();
-        if (minion == null) return;
-        minion.DamageDebuff(true, _speedDelta);
+        foreach (var mesh in _meshModified)
+        {
+            mesh.enabled = !mesh.enabled;
+        }
     }
     
+    void TriggerEnterHandler(Collider col)
+    {
+
+        if (col.gameObject.layer == LayerMask.NameToLayer("Minion"))
+        {
+            var minion = col.GetComponent<Minion>();
+            if (minion == null) return;
+            minion.DamageDebuff(true, _speedDelta);
+        }
+        else if (col.gameObject.layer == LayerMask.NameToLayer("MinionPath"))
+        {
+            EnableMesh(col);
+        }
+        //Debug.Log("TriggerEnterHandler");
+        
+    }
+
+    void EnableMesh(Collider col)
+    {
+        var mesh = col.GetComponent<MeshRenderer>();
+        if (mesh != null)
+        {
+            mesh.enabled = !mesh.enabled;
+            _meshModified.Add(mesh);
+        }
+    }
 
     void TriggerExitHandler(Collider col)
     {
-        if (col.gameObject.layer != LayerMask.NameToLayer("Minion")) return;
+        Debug.Log(col.name);
+        if (col.gameObject.layer == LayerMask.NameToLayer("Minion"))
+        {
+            var minion = col.GetComponent<Minion>();
+            if (minion == null) return;
+            minion.DamageDebuff(false);
+        }
         //Debug.Log("TriggerExitHandler");
-        var minion = col.GetComponent<Minion>();
-        if (minion == null) return;
-        minion.DamageDebuff(false);
+
     }
 }
