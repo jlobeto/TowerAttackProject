b61c3c252713a78e0ba83b9a882e14ba91572b8c
Ales <ales-punk@hotmail.com>
2018-10-30 00:08:07 -0300

    Clase 29-10

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 3;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update ()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        var zoom = new Vector3(x, 0, z);
+        transform.position += Time.deltaTime * speed * zoom * 5; 
+    }
+}
