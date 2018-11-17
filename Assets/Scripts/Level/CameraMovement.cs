1923c18533f9c8a4f02fc9e4846c2eebe7066ee6
jlobetojc <jlobeto@jamcity.com>
2018-11-17 02:16:07 -0300

    Velocidad de la camara, habilitado la visual del rango del canon, y activadas las torres(desactivadas y commiteadas por error) del lvl 1

--- Assets/Scripts/Level/CameraMovement.cs
+++ Assets/Scripts/Level/CameraMovement.cs
@@ -21,8 +21,8 @@ public class CameraMovement : MonoBehaviour
 	{
 		var x = Input.GetAxis("Horizontal");
 		var z = Input.GetAxis("Vertical");
-		transform.position += Time.deltaTime * speed * z * transform.forward * 5;
-		transform.position += Time.deltaTime * speed * x * transform.right * 5;
+		transform.position += Time.deltaTime * speed * z * transform.forward * 10;
+		transform.position += Time.deltaTime * speed * x * transform.right * 10;
 	}
 
 	void CheckFingers()
@@ -35,7 +35,7 @@ public class CameraMovement : MonoBehaviour
 			if (t1.phase == TouchPhase.Moved)// && t2.phase == TouchPhase.Moved) 
 			{
 				var deltaPos = t1.deltaPosition;
-				transform.Translate (new Vector3 (-deltaPos.x * (speed / 3), 0 , -deltaPos.y * (speed / 3)));
+				transform.Translate (new Vector3 (-deltaPos.x * (speed / 15), 0 , -deltaPos.y * (speed / 15)));
 			}
 		}
 	}
