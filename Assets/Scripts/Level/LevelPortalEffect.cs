5841baddc7bad419030a49586c537c73d97cb255
Julian Lobeto <julian.lobeto93@gmail.com>
2018-11-24 16:17:32 -0300

    Ahora lanza un burst the particulas cuando un minion pasa por el portal de salida

---
+++ Assets/Scripts/Level/LevelPortalEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPortalEffect : MonoBehaviour
+{
+
+    public ParticleSystem portalPS;
+
+	void Start ()
+    {
+	    	
+	}
+	
+	
+	void Update () {
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Minion"))
+        {
+            portalPS.Emit(15);
+        }
+    }
+}
