7924d354c1eec014be747487917b7fddc3705b02
Julian Lobeto <julian.lobeto93@gmail.com>
2018-12-02 00:52:55 -0300

    Electroshock Tutorial (lvl5)

---
+++ Assets/Scripts/Level/Tutorial/TutorialElectroshockSteps/ShowElectroshockText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowElectroshockText : StepBase
+{
+    TutorialElectroshockManager _tutoMan;
+    string _textName;
+
+    public ShowElectroshockText(string textName, TutorialElectroshockManager t)
+    {
+        _textName = textName;
+        _tutoMan = t;
+    }
+
+    public override void ExecuteStep(GameObject gameObject = null)
+    {
+        _tutoMan.canvasTuto.DisableTexts();
+        _tutoMan.canvasTuto.EnableDisableTextByName(_textName, true);
+    }
+}
---
+++ Assets/Scripts/Level/Tutorial/TutorialElectroshockSteps/StopElectroshockTuto.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopElectroshockTuto : StepBase
+{
+    TutorialElectroshockManager _tutoMan;
+    CameraMovement _camMovement;
+
+
+    public StopElectroshockTuto( TutorialElectroshockManager t)
+    {
+        _tutoMan = t;
+        _camMovement = GameObject.FindObjectOfType<CameraMovement>();
+
+    }
+
+    public override void ExecuteStep(GameObject gameObject = null)
+    {
+        _tutoMan.lvl.isTutorial = false;
+        Time.timeScale = 1;
+
+        _tutoMan.canvasTuto.DisableTexts();
+        _tutoMan.canvasTuto.DisableAllArrows();
+
+        _tutoMan.CanClick = false;
+
+        _camMovement.SetCameraMovement(true);
+
+    }
+}
