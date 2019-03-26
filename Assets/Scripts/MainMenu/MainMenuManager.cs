685ca35515c0accc73811646114ccb2c6aad0f54
Julian Lobeto <julian.lobeto93@gmail.com>
2019-03-25 22:04:46 -0300

    Main Menu with buttons working ( play and exit)

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+
+    public void GoToMainMap()
+    {
+        SceneManager.LoadScene("NewMainMap");
+    }
+
+    public void ExitApp()
+    {
+        if(Application.isEditor)
+            UnityEditor.EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+}
