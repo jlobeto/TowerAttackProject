303cab6c37d1cec4699cb6aee99c30dd142b39b2
Julian Lobeto <julian.lobeto93@gmail.com>
2019-06-24 23:06:23 -0300

    sadasd

--- Assets/Scripts/Level/DetailsManager.cs
+++ Assets/Scripts/Level/DetailsManager.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 
 public class DetailsManager : MonoBehaviour
 {
-    List<Material> materials;
-    Color _baseColor;
+    public Color _baseColor;
 
     // Start is called before the first frame update
     void Start()
     {
-        //_baseColor = materials[0].GetColor("_EmissionColor");
-
         foreach (Transform item in transform)
         {
             if(item.tag == "point")
             {
-                item.GetComponent<Material>().
+                var mat = item.GetComponent<Renderer>().material;
+
+                /*if (_baseColor == null)
+                    _baseColor = mat.GetColor("_EmissionColor");
+                    */
+                mat.SetColor("_EmissionColor", Color.black);
             }
         }
-        /*
-        foreach (var item in materials)
-        {
-            item.SetColor("_EmissionColor", Color.black);
-        }*/
+        
+        StartCoroutine(TurnOn());
     }
+
+    IEnumerator TurnOn()
+    {
+        while(true)
+        {
+            foreach (Transform item in transform)
+            {
+                if (item.tag == "point")
+                {
+                    var mat = item.GetComponent<Renderer>().material;
+                    mat.SetColor("_EmissionColor", _baseColor);
+                    StartCoroutine(TurnOff(mat));
+                    yield return new WaitForSeconds(.05f);
+                }
+            }
+        }
+        
+    }
+
+    IEnumerator TurnOff(Material item)
+    {
+        yield return new WaitForSeconds(.5f);
+        item.SetColor("_EmissionColor", Color.black);
+    }
+
+
 
     // Update is called once per frame
     void Update()
