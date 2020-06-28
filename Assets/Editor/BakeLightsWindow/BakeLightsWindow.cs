129b792909363b029ec5d0d4e27530d78471daaa
Julian Lobeto <julian.lobeto93@gmail.com>
2020-06-28 18:06:10 -0300

    bake light window!

--- Assets/Editor/BakeLightsWindow/BakeLightsWindow.cs
+++ Assets/Editor/BakeLightsWindow/BakeLightsWindow.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
+[ExecuteInEditMode]
 public class BakeLightsWindow : EditorWindow
 {
     string _pathToScenes = "Assets/Scenes/Levels";
     string[] _lightmapResolution = new string[] { "1024", "2048" };
     int _lightMapResIndex;
+    bool _baking;
+    string[] _realPaths;
+    int _currentSceneBuildingIndex = 0;
+    List<string> _scenesBuilt = new List<string>();
 
     [MenuItem("UTILITIES/Bake Scenes Lights")]
     static void Init()
     {
-
         BakeLightsWindow window = (BakeLightsWindow)EditorWindow.GetWindow(typeof(BakeLightsWindow));
         window.Show();
+    }
+
+    void Update()
+    {
+        if (!_baking) return;
+
+        if(!Lightmapping.isRunning)
+        {
+            _scenesBuilt.Add(_realPaths[_currentSceneBuildingIndex]); 
+            EditorSceneManager.OpenScene(_realPaths[_currentSceneBuildingIndex]);
+
+            LightmapEditorSettings.maxAtlasSize = int.Parse(_lightmapResolution[_lightMapResIndex]);
+            LightmapEditorSettings.lightmapper = LightmapEditorSettings.Lightmapper.ProgressiveCPU;
+            LightmapEditorSettings.mixedBakeMode = MixedLightingMode.Subtractive;
+            LightmapEditorSettings.lightmapsMode = LightmapsMode.NonDirectional;
+            LightmapEditorSettings.filteringMode = LightmapEditorSettings.FilterMode.Auto;
+            Lightmapping.realtimeGI = false;
+
+            Lightmapping.BakeAsync();
+            _currentSceneBuildingIndex++;
+
+            if (_realPaths.Length == _currentSceneBuildingIndex)
+                _baking = false;
+        }
+        
     }
 
     void OnGUI()
@@ -27,28 +57,43 @@ public class BakeLightsWindow : EditorWindow
         GUILayout.Space(1);
 
 
-        if (GUILayout.Button("BAKE LIGHTS !"))
+        if (GUILayout.Button("BAKE LIGHTS !") && !_baking)
         {
+            _baking = true;
             BakeLights();
+        }
+
+        if(_baking)
+        {
+            EditorGUILayout.LabelField("Scenes Already Baked and Baking:");
+            foreach (var item in _scenesBuilt)
+            {
+                var splitted = item.Split('/');
+                EditorGUILayout.LabelField(splitted[splitted.Length - 1]);
+            }
+            if (GUILayout.Button("STOP BAKING !"))
+            {
+                Lightmapping.ForceStop();
+                _baking = false;
+            }
         }
     }
 
     void BakeLights()
     {
         var paths = new string[] { _pathToScenes };
-        LightmapEditorSettings.maxAtlasSize = int.Parse(_lightmapResolution[_lightMapResIndex]);
-        LightmapEditorSettings.lightmapper = LightmapEditorSettings.Lightmapper.ProgressiveCPU;
-        LightmapEditorSettings.mixedBakeMode = MixedLightingMode.Subtractive;
-        LightmapEditorSettings.lightmapsMode = LightmapsMode.NonDirectional;
 
         var p = AssetDatabase.FindAssets("t:scene", paths);
         var list = new List<string>();
+        
         foreach (var item in p)
         {
             var i = AssetDatabase.GUIDToAssetPath(item);
             list.Add(i);
         }
 
-        Lightmapping.BakeMultipleScenes(list.ToArray());
+        _realPaths = list.ToArray();
+        _currentSceneBuildingIndex = 0;
+        _scenesBuilt = new List<string>();
     }
 }
